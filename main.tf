5093fbc9138ec708ff1e27d6ab83eb2edbbb4c67
ZahiMaster <50413498+ZahiMaster@users.noreply.github.com>
2020-12-13 23:49:55 -0300

    Commit3

--- main.tf
+++ main.tf
@@ -1,10 +1,14 @@
 provider "aws" {
-  region  = "us-east-1"
-  version = "~> 2.0"
+    region = "us-east-1"
+}
+
+provider "aws" {
+    alias = "west"
+    region = "us-west-2"
 }
 
 terraform {
-  beckend "s3" {
+  backend "s3" {
     #Meu bucket para guardar o tfstate !@@@@
     bucket = "bucket-terraform-zahi-master"
     key    = "terraform-test.tfstate"
