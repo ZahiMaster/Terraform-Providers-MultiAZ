3612e8ab9e82a9ced2ef4472b6dd7b14e0606f15
ZahiMaster <50413498+ZahiMaster@users.noreply.github.com>
2020-12-13 23:14:14 -0300

    Subindo arquivo main

---
+++ mail.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region  = "us-east-1"
+  version = "~> 2.0"
+}
+
+terraform {
+  beckend "s3" {
+    #Meu bucket para guardar o tfstate
+    bucket = "bucket-terraform-zahi-master"
+    key    = "terraform-test.tfstate"
+    region = "us-east-1"
+  }
+}
